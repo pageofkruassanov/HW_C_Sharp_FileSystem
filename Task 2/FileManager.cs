30f77d357fb0942a7a1fc9c706a115a92a940297
Pavlo <pkhasanov@gmail.com>
2023-10-22 23:09:36 +0300

    Add project files.

---
+++ Task 2/FileManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Task_2
+{
+    internal class FileManager
+    {
+        public void SaveToFile(Path dir, string data)
+        {
+            if (!Directory.Exists(dir.DirectoryPath)) { Directory.CreateDirectory(dir.DirectoryPath); }
+
+            string jsonData = JsonConvert.SerializeObject(data);
+            File.WriteAllText(dir.FilePath, jsonData);
+        }
+
+        public string LoadFromFile(Path dir)
+        {
+            if (File.Exists(dir.FilePath))
+            {
+                string jsonData = File.ReadAllText(dir.FilePath);
+                string data = JsonConvert.DeserializeObject<string>(jsonData);
+
+                return data;
+            }
+            else { throw new FileNotFoundException("File not found."); }
+        }
+    }
+}
---
+++ Task 2/Path.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Path
+    {
+        private string _directoryPath;
+
+        private string _filePath;
+
+        public Path()
+        {
+            _directoryPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        public string DirectoryPath
+        {
+            get { return _directoryPath; }
+        }
+
+        public string FilePath
+        {
+            set { _filePath = System.IO.Path.Combine(_directoryPath, value); }
+            get { return _filePath; }
+        }
+    }
+}
